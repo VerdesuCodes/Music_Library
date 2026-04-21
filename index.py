1cda9358c8a87c1961df39afdfe2bc3c4d162e2c
VerdesuCodes <and_val@hotmail.com>
2026-04-21 17:50:21 -0400

    load_musics encoded with utf-8 (problems with accents), and list musics (for function to write all itens in the array on the JSON)

--- index.py
+++ index.py
@@ -1,1 +1,23 @@
-print('Hello World')
+import json
+
+def load_musics():
+    with open("musics.json", "r", encoding="utf-8") as m:
+        return json.load(m)
+    
+def list_musics(musics):
+    for music in musics:
+        print(music)
+
+list = load_musics()
+list_musics(list)
+
+
+
+
+
+#menu de opcoes
+
+
+#1. Adicionar musica
+#2. Listar musicas
+#3. Remover musica
